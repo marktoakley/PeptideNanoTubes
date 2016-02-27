13a558cb79abd5e69a20f37bc51ee356b03a79db
Mark Oakley <marktoakley@hotmail.com>
2016-02-27 14:24:26 +0000

    Added build_tube() function.
    Added build_tube(), which constructs parallel or antiparallel nanotubes.

--- tubemaker/nanotube.py
+++ tubemaker/nanotube.py
@@ -63,6 +63,28 @@ def build_ring(num_res, res_coords):
         coords = np.append(coords,res_coords,axis=0)
     return coords
 
+def build_tube(num_rings, num_res, res_coords, parallel=True, spacing=5.0):
+    '''Construct the coordinates of a cyclic peptide ring.
+    Parameters:
+    num_rings:
+    num_res: The number of residues in the ring
+    res_coords: The coordinates of a peptide residue (see orient_coords)'''
+    ring_coords = build_ring(num_res, res_coords)
+    tube_coords = ring_coords.copy()
+    shift = np.array([0.,0.,spacing])
+    for i in range(num_rings-1):
+        ring_coords=ring_coords+shift
+        if not parallel:
+            ring_coords = ring_coords*np.array([-1.,1.,1.])
+            theta = 2*pi/num_res
+            rot = np.array([[cos(theta),-sin(theta),0],
+                            [sin(theta), cos(theta),0],
+                            [         0,  0,1]])
+            ring_coords=ring_coords.dot(rot)
+        tube_coords = np.append(tube_coords,ring_coords,axis=0)
+    return tube_coords
+        
+
 def write_amber_coords(coords,res_name,file_name="coords.inpcrd"):
     '''Write an Amber inpcrd file containing the nanotube's coordinates.'''
     with open(file_name,'w') as inpcrd:
@@ -78,8 +100,10 @@ if __name__ == "__main__":
     amber_home=os.environ.get('AMBERHOME')
     lib = amber_home+"/dat/leap/lib/all_amino03.lib"
     num_res = 8
+    num_rings = 4
     res_name="ALA"
     res_coords =  read_amber_coords(res_name, lib)
     res_coords = orient_coords(res_coords)
-    coords = build_ring(num_res, res_coords)
+    #coords = build_ring(num_res, res_coords)
+    coords = build_tube(num_rings, num_res, res_coords,parallel = False)
     write_amber_coords(coords,res_name)
